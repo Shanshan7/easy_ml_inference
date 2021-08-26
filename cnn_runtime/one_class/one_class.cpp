b348649dab9768b5a328de12b3bce54ca5b49630
foww-0001 <1336946993@qq.com>
2021-08-26 13:11:42 +0800

    fix bug

--- cnn_runtime/one_class/one_class.cpp
+++ cnn_runtime/one_class/one_class.cpp
@@ -7,15 +7,24 @@
 #include <opencv2/highgui.hpp>
 #include <opencv2/ml.hpp>
 
+#include <sys/time.h>
+
 #define KNEIGHBOURS (9)
-#define LENGTH (2515968)
+#define FEATUREWIDTH (1638)
+#define FEATUREHEIGHT (1536)
+
+unsigned long get_current_time(void)
+{
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    return (tv.tv_sec*1000000 + tv.tv_usec);
+}
 
 void reshape_embedding(const cv::Mat &embedding_array, cv::Mat embedding_test)
 {
     int out_channel = embedding_array.size[1];
     int out_height = embedding_array.size[2];
     int out_width = embedding_array.size[3];
-    std::cout << out_channel << " " << out_height << " " << out_width << std::endl;
     for (int h = 0; h < out_height; h++)
     {
         for (int w = 0; w < out_width; w++)
@@ -55,7 +64,7 @@ void preprocess(const cv::Mat &src_mat, const cv::Size dst_size, cv::Mat &dst_im
 
 float postprocess(const cv::Mat &embedding_coreset, const cv::Mat &embedding_test)
 {
-    const int K(9);
+    const int K(KNEIGHBOURS);
     cv::Ptr<cv::ml::KNearest> knn = cv::ml::KNearest::create();
     knn->setDefaultK(K);
     knn->setAlgorithmType(cv::ml::KNearest::BRUTE_FORCE);
@@ -63,55 +72,44 @@ float postprocess(const cv::Mat &embedding_coreset, const cv::Mat &embedding_tes
 
     knn->train(embedding_coreset, cv::ml::ROW_SAMPLE, labels);
 
-    cv::Mat result, neighborResponses, distances_a;
-    knn->findNearest(embedding_test, K, result, neighborResponses, distances_a);
-    // std::cout << "result: " << distances.size[0] << " " << distances.size[1] << std::endl;
+    cv::Mat result, neighborResponses, distances_mat;
+    knn->findNearest(embedding_test, K, result, neighborResponses, distances_mat);
 
-    float* distances = new float[784*9];
-    memcpy(distances, distances_a.data, sizeof(float)*784*9);
+    int distanceMatWidth = distances_mat.size[0];
+    int distanceMatHeight = distances_mat.size[1];
+    // std::cout << "result: " << distances_mat.size[0] << " " << distances_mat.size[1] << std::endl;
 
-    // float* distances_numpy = new float[784*9];
-    // std::ifstream distance_f;
-    // distance_f.open("/home/edge/easy_data/easy_ml_inference/cnn_runtime/one_class/distances.bin", std::ifstream::binary);
-    // distance_f.read(reinterpret_cast<char*>(distances_numpy), sizeof(float) * 784 * 9);
-    // distance_f.close();
-
-    // float* distances = new float[784*9];
-    // for (int d = 0; d < 9; d++)
-    // {
-    //     for (int c = 0; c < 784; c++)
-    //     {
-    //         memcpy(distances + d*784 + c, distances_numpy + c*9 + d, sizeof(float));
-    //     }
-    // }
-
-    // for (int b = 0; b < 20; b++)
-    // {
-    //     std::cout << distances[b] << " ";
-    // }
-
-    // float* max_neighbours = new float[28*28];
-    // memcpy(max_neighbours, distances + 28 * 28, sizeof(float)*28*28);
-    int max_posit = std::max_element(distances, \
-				                distances + 28 * 28) - distances; // - distances.data;
-
-    std::cout << "max_posit: " << max_posit << std::endl;
-
-    float* N_b = new float[9];
-    for(int i = 0; i < 9; i++)
+    // reshape 784 * 9 --> 9 * 784
+    float* distances = new float[distanceMatWidth*distanceMatHeight];
+    for (int d = 0; d < distanceMatHeight; d++)
     {
-        N_b[i] = distances[i * 28 * 28 + max_posit];
-        // N_b[i] = ((float*)distances.data)[i * 28 * 28 + max_posit];
+        for (int c = 0; c < distanceMatWidth; c++)
+        {
+            distances[d*distanceMatWidth + c] = ((float*)distances_mat.data)[c*distanceMatHeight + d];
+            // memcpy(distances + d*784 + c, distances_mat.data + c*9 + d, sizeof(float));
+        }
     }
+
+    int max_posit = std::max_element(distances, \
+				                distances + distanceMatWidth) - distances; // - distances.data;
+
+    // std::cout << "max_posit: " << max_posit << std::endl;
+
+    float* N_b = new float[distanceMatHeight];
+    for(int i = 0; i < distanceMatHeight; i++)
+    {
+        N_b[i] = distances[i * distanceMatWidth + max_posit];
+    }
+
     float w, sum_N_b = 0;
-    for(int j = 0; j < 9; j++)
+    for(int j = 0; j < distanceMatHeight; j++)
     {
         sum_N_b += exp(N_b[j]);
     }
-    float max_N_b = *std::max_element(N_b, N_b + 9);
+    float max_N_b = *std::max_element(N_b, N_b + distanceMatHeight);
     w = (1 - exp(max_N_b) / sum_N_b);
+
     float score;
-    // score = w * ((float*)distances.data)[max_posit];
     score = w * distances[max_posit];
 
     return score;
@@ -120,16 +118,14 @@ float postprocess(const cv::Mat &embedding_coreset, const cv::Mat &embedding_tes
 int main()
 {
     std::string model_file = "/home/edge/easy_data/easy_ml_inference/cnn_runtime/one_class/OneClassNet.onnx";
-    std::string image_file = "/home/edge/easy_data/easy_ml_inference/cnn_runtime/one_class/000.png";
-    //"/easy_data/easy_ml_inference/cnn_runtime/one_class/000.png";
     std::string image_txt_path = "/home/edge/data/VOCdevkit/MVtec/bottle/ImageSets/val.txt";
     std::string image_dir = "/home/edge/data/VOCdevkit/MVtec/bottle/JPEGImages/";
     std::string embedding_file = "/home/edge/easy_data/easy_ml_inference/cnn_runtime/one_class/embedding.bin";
 
-    float* embedding_coreset = new float[LENGTH];
+    float* embedding_coreset = new float[FEATUREWIDTH * FEATUREHEIGHT];
     std::ifstream embedding;
     embedding.open(embedding_file, std::ifstream::binary);
-    embedding.read(reinterpret_cast<char*>(embedding_coreset), sizeof(float) * LENGTH);
+    embedding.read(reinterpret_cast<char*>(embedding_coreset), sizeof(float) * FEATUREWIDTH * FEATUREHEIGHT);
     embedding.close();
 
     cv::dnn::Net net = cv::dnn::readNetFromONNX(model_file);
@@ -163,48 +159,25 @@ int main()
         int out_channel = out.size[1];
         int out_height = out.size[2];
         int out_width = out.size[3];
-        // for (int c = 0; c < 20; c++)
-        // {
-        //     std::cout << ((float*)out.data)[c] << " ";
-        // }
-        // std::cout << std::endl;
 
         float score;
-        cv::Mat embedding_train(LENGTH/out_channel, out_channel, CV_32FC1);
+        cv::Mat embedding_train(FEATUREWIDTH, FEATUREHEIGHT, CV_32FC1);
         cv::Mat embedding_test(out_height*out_width, out_channel, CV_32FC1);
 
-        memcpy(embedding_train.data, embedding_coreset, LENGTH*sizeof(float));
+        memcpy(embedding_train.data, embedding_coreset, FEATUREWIDTH * FEATUREHEIGHT * sizeof(float));
         reshape_embedding(out, embedding_test);
-
-        // for (int c = 0; c < 5; c++)
+       
+        // for (int a = 0; a < 20; a++)
         // {
-        //     std::cout << embedding_train.at<float>(c, 0) << " ";
+        //     std::cout << ((float*)embedding_test.data)[a] << " ";
         // }
-        // std::cout << std::endl;
-        // for (int c = 0; c < 20; c++)
-        // {
-        //     std::cout << ((float*)embedding_train.data)[c] << " ";
-        // }
-        // std::cout << std::endl;
-        // for (int c = 0; c < 20; c++)
-        // {
-        //     std::cout << ((float*)embedding_test.data)[c] << " ";
-        // }
-        // std::cout << std::endl;
+        // std::cout << "\n";
 
-        // float* embedding_numpy = new float[out_height*out_width*out_channel];
-        // std::ifstream embedding_f;
-        // embedding_f.open("/home/edge/easy_data/easy_ml_inference/cnn_runtime/one_class/embedding_test.bin", std::ifstream::binary);
-        // embedding_f.read(reinterpret_cast<char*>(embedding_numpy), sizeof(float)*out_height*out_width*out_channel);
-        // embedding_f.close();
-
-        // for (int b = 0; b < 20; b++)
-        // {
-        //     std::cout << embedding_coreset[b] << " ";
-        // }
-        // memcpy(embedding_test.data, embedding_numpy, sizeof(float)*out_height*out_width*out_channel);
-
+        unsigned long time_start, time_end;
+        time_start = get_current_time();
         score = postprocess(embedding_train, embedding_test);
+        time_end = get_current_time();
+        std::cout << "one_class_net cost time: " <<  (time_end - time_start)/1000.0  << "ms" << std::endl;
         std::cout << "score: " << score << std::endl;
     }
 
