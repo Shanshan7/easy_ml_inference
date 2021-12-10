39c83d25a30ff6c448bb68f4abfca09ca520a672
foww-0001 <1336946993@qq.com>
2021-12-10 14:51:51 +0800

    加入deepsort可以使用

---
+++ cnn_runtime/yolov5rt/build/CMakeFiles/test_yolov5rt.dir/DependInfo.cmake
@@ -0,0 +1,37 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/edge/easy_data/easy_ml_inference/cnn_runtime/yolov5rt/deepsort/deepsort.cpp" "/home/edge/easy_data/easy_ml_inference/cnn_runtime/yolov5rt/build/CMakeFiles/test_yolov5rt.dir/deepsort/deepsort.cpp.o"
+  "/home/edge/easy_data/easy_ml_inference/cnn_runtime/yolov5rt/deepsort/featuretensor.cpp" "/home/edge/easy_data/easy_ml_inference/cnn_runtime/yolov5rt/build/CMakeFiles/test_yolov5rt.dir/deepsort/featuretensor.cpp.o"
+  "/home/edge/easy_data/easy_ml_inference/cnn_runtime/yolov5rt/deepsort/hungarianoper.cpp" "/home/edge/easy_data/easy_ml_inference/cnn_runtime/yolov5rt/build/CMakeFiles/test_yolov5rt.dir/deepsort/hungarianoper.cpp.o"
+  "/home/edge/easy_data/easy_ml_inference/cnn_runtime/yolov5rt/deepsort/kalmanfilter.cpp" "/home/edge/easy_data/easy_ml_inference/cnn_runtime/yolov5rt/build/CMakeFiles/test_yolov5rt.dir/deepsort/kalmanfilter.cpp.o"
+  "/home/edge/easy_data/easy_ml_inference/cnn_runtime/yolov5rt/deepsort/linear_assignment.cpp" "/home/edge/easy_data/easy_ml_inference/cnn_runtime/yolov5rt/build/CMakeFiles/test_yolov5rt.dir/deepsort/linear_assignment.cpp.o"
+  "/home/edge/easy_data/easy_ml_inference/cnn_runtime/yolov5rt/deepsort/munkres.cpp" "/home/edge/easy_data/easy_ml_inference/cnn_runtime/yolov5rt/build/CMakeFiles/test_yolov5rt.dir/deepsort/munkres.cpp.o"
+  "/home/edge/easy_data/easy_ml_inference/cnn_runtime/yolov5rt/deepsort/nn_matching.cpp" "/home/edge/easy_data/easy_ml_inference/cnn_runtime/yolov5rt/build/CMakeFiles/test_yolov5rt.dir/deepsort/nn_matching.cpp.o"
+  "/home/edge/easy_data/easy_ml_inference/cnn_runtime/yolov5rt/deepsort/track.cpp" "/home/edge/easy_data/easy_ml_inference/cnn_runtime/yolov5rt/build/CMakeFiles/test_yolov5rt.dir/deepsort/track.cpp.o"
+  "/home/edge/easy_data/easy_ml_inference/cnn_runtime/yolov5rt/deepsort/tracker.cpp" "/home/edge/easy_data/easy_ml_inference/cnn_runtime/yolov5rt/build/CMakeFiles/test_yolov5rt.dir/deepsort/tracker.cpp.o"
+  "/home/edge/easy_data/easy_ml_inference/utility/utils.cpp" "/home/edge/easy_data/easy_ml_inference/cnn_runtime/yolov5rt/build/CMakeFiles/test_yolov5rt.dir/home/edge/easy_data/easy_ml_inference/utility/utils.cpp.o"
+  "/home/edge/easy_data/easy_ml_inference/cnn_runtime/yolov5rt/test_yolov5rt.cpp" "/home/edge/easy_data/easy_ml_inference/cnn_runtime/yolov5rt/build/CMakeFiles/test_yolov5rt.dir/test_yolov5rt.cpp.o"
+  "/home/edge/easy_data/easy_ml_inference/cnn_runtime/yolov5rt/yolov5/yolov5.cpp" "/home/edge/easy_data/easy_ml_inference/cnn_runtime/yolov5rt/build/CMakeFiles/test_yolov5rt.dir/yolov5/yolov5.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../common"
+  "../yolov5"
+  "../deepsort"
+  "../../../3rd_party"
+  "../../../utility"
+  "/usr/local/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cnn_runtime/yolov5rt/build/CMakeFiles/test_yolov5rt.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_yolov5rt.dir/test_yolov5rt.cpp.o"
+  "CMakeFiles/test_yolov5rt.dir/deepsort/deepsort.cpp.o"
+  "CMakeFiles/test_yolov5rt.dir/deepsort/featuretensor.cpp.o"
+  "CMakeFiles/test_yolov5rt.dir/deepsort/hungarianoper.cpp.o"
+  "CMakeFiles/test_yolov5rt.dir/deepsort/kalmanfilter.cpp.o"
+  "CMakeFiles/test_yolov5rt.dir/deepsort/linear_assignment.cpp.o"
+  "CMakeFiles/test_yolov5rt.dir/deepsort/munkres.cpp.o"
+  "CMakeFiles/test_yolov5rt.dir/deepsort/nn_matching.cpp.o"
+  "CMakeFiles/test_yolov5rt.dir/deepsort/track.cpp.o"
+  "CMakeFiles/test_yolov5rt.dir/deepsort/tracker.cpp.o"
+  "CMakeFiles/test_yolov5rt.dir/yolov5/yolov5.cpp.o"
+  "CMakeFiles/test_yolov5rt.dir/home/edge/easy_data/easy_ml_inference/utility/utils.cpp.o"
+  "test_yolov5rt.pdb"
+  "test_yolov5rt"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_yolov5rt.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
